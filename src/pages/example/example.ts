aafac35894b79b7fe5f2ca7940c6d11aba0ec3e4
Mujkic Ohran <ohran.mujkic@bfh.ch>
2018-05-07 11:48:03 +0200

    Initial commit

---
+++ src/pages/example/example.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-example',
+  templateUrl: 'example.html'
+})
+export class ExamplePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
