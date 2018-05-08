c5481b36f02ed57ed6134397d4124b40b491e4fe
Unknown <besio1@bfh.ch>
2018-05-08 13:49:28 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/pages/sehtestHome/sehtestHome.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-sehtestHome',
+  templateUrl: 'sehtestHome.html'
+})
+export class SehtestHomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
